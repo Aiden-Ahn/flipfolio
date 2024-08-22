2356663baceb589716ac22fe775351ac3cf484e4
aiden <aiden.c.ahn@gmail.com>
2024-08-22 15:10:35 +1000

    first landing page

--- landingapp/views.py
+++ landingapp/views.py
@@ -1,9 +1,26 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.urls import reverse
+
+from landingapp.models import EmailList
 
 # Create your views here.
 
 
 def landingpage(request):
-    return render(request, 'landingapp/landing.html')
+
+    if request.method == "POST":
+
+        temp = request.POST.get('email_input')
+
+        new_email = EmailList()
+        new_email.text = temp
+        new_email.save()
+
+        email_output_list = EmailList.objects.all()
+
+        return HttpResponseRedirect(reverse('landingapp:landingpage'))
     
+    else:
+        email_output_list = EmailList.objects.all()
+        return render(request, 'landingapp/landing.html', context={'email_output_list': email_output_list})
